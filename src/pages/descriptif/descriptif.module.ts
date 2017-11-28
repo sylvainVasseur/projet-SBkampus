98c956d05513120b0c4a1b80f9c747b8e37ee794
local192 <local192@lisaac13.univ-lr.fr>
2017-11-28 13:51:31 +0100

    ajout page descriptif
    ajout de la page descriptif

---
+++ src/pages/descriptif/descriptif.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DescriptifPage } from './descriptif';
+
+@NgModule({
+  declarations: [
+    DescriptifPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DescriptifPage),
+  ],
+})
+export class DescriptifPageModule {}
