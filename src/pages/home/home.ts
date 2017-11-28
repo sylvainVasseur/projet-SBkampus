f7896067192d631bee4193ace8b086df86c2fbea
Bastien Vissac <VissacBastien@gmail.com>
2017-11-28 14:56:18 +0100

    Conflits

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
-import { DestinatairePage } from "../destinataire/destinataire";
-=======
 import { DestinatairePage } from '../destinataire/destinataire';
->>>>>>> 4c18341e93e15bbd1eb61d57f5dd3f077462d41f
 
 @Component({
   selector: 'page-home',
@@ -15,12 +11,8 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  
   OpenDest(){
     this.navCtrl.push(DestinatairePage);
   }
-
-  openDest(){
-    this.navCtrl.push(DestinatairePage);
-  }
-
 }
