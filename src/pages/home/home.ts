98a15cce9eafd7df0a71fbe85f5746406ee91f0a
local192 <local192@lisaac13.univ-lr.fr>
2017-11-28 14:59:50 +0100

    class

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-<<<<<<< HEAD
+
 import { DestinatairePage } from "../destinataire/destinataire";
-=======
-import { DestinatairePage } from '../destinataire/destinataire';
->>>>>>> 4c18341e93e15bbd1eb61d57f5dd3f077462d41f
+
 
 @Component({
   selector: 'page-home',
