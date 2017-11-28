165bb23f5059e19273dfe7f5019e3aca9704ac40
local192 <local192@lisaac13.univ-lr.fr>
2017-11-28 14:46:13 +0100

    fonction modif page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DestinatairePage } from "../destinataire/destinataire";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  OpenDest(){
+    this.navCtrl.push(DestinatairePage);
   }
 
 }
