28199e8d99bd4e976ebd23920405f1179b2c0f43
local192 <local192@lisaac13.univ-lr.fr>
2017-11-29 09:48:34 +0100

    update tableau destinataire

--- src/pages/destinataire/destinataire.ts
+++ src/pages/destinataire/destinataire.ts
@@ -8,9 +8,30 @@ import { ToastController } from 'ionic-angular';
   templateUrl: 'destinataire.html',
 })
 export class DestinatairePage {
+ListDestinataire:Array<{destinataire: string}>;
 
   constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
+    this.ListDestinataire = [
+      {
+        "destinataire": "Direction du systeme d information",
+      },
+      {
+        "destinataire": "IUT Service Informatique",
+      },
+      {
+        "destinataire": "Service Informatique Transversale",
+      },
+      {
+        "destinataire": "Sonel",
+      },
+      {
+        "destinataire": "STU Maintenance",
+      },
+      {
+        "destinataire": "@ctice",
+      }
+    ];
   }
   //Toast pour la prochaine page
   showToast(position: string) {
