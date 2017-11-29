5f9404648f1c5a720b7b59a641f63ec974410e77
Bastien Vissac <VissacBastien@gmail.com>
2017-11-29 14:30:26 +0100

    Revert "destinataire pas de checkbox"
    This reverts commit e70fef3652661a071dc853e88cf10ae5a81085c9.

--- src/pages/destinataire/destinataire.ts
+++ src/pages/destinataire/destinataire.ts
@@ -32,7 +32,6 @@ export class DestinatairePage {
         destinataire: "@ctice",
       }
     ];
-
   }
 
   //Toast pour la prochaine page
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -118,7 +118,6 @@ export class HomePage {
         descriptif:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte."
       },
     ];
-
   }
 
   //Toast pour la prochaine page
