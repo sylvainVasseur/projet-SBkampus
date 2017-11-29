526a505d6dfa03a6fe36cb03d98ba6e6ee3ddf4e
Bastien Vissac <VissacBastien@gmail.com>
2017-11-29 12:04:23 +0100

    Quelques trucs

--- src/pages/descriptif/descriptif.ts
+++ src/pages/descriptif/descriptif.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { LocalisationPage } from '../localisation/localisation';
 import { MediaPage } from '../media/media';
-
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-descriptif',
@@ -9,7 +10,7 @@ import { MediaPage } from '../media/media';
 })
 export class DescriptifPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
   }
 
@@ -18,8 +19,37 @@ export class DescriptifPage {
     month: 'today'
   }
 
+  //Toast pour la localisation
+  showToastLoca(position: string) {
+    let toast = this.toastCtrl.create({
+    message: 'Scanner le QRCode',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
+  //Bouton ajout QRCode
+  OpenLoca(){
+    this.navCtrl.push(LocalisationPage);
+    this.showToastLoca('down');
+  }
+
+  //Toast pour la prochaine page
+  showToast(position: string) {
+    let toast = this.toastCtrl.create({
+    message: 'Ajouter une photo',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
   //Fonction pour le clic
   OpenMed(){
       this.navCtrl.push(MediaPage);
+      this.showToast('down');
   }
 }
--- src/pages/media/media.ts
+++ src/pages/media/media.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MediaPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
 
   }
 
