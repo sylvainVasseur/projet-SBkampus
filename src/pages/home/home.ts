e62f977525cc357d179225b3e613dd259fbf18b6
Bastien Vissac <VissacBastien@gmail.com>
2018-01-17 16:23:41 +0100

    Pas de s à Licence

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,7 +33,7 @@ export class HomePage {
       },
       {
         numero: "3",
-        nom:"Liscence Apple",
+        nom:"Licence Apple",
         date: "15/12/17",
         destinataire:"Direction des services informatiques",
         descriptif:"Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
